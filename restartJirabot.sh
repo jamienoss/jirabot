a95c3e820a6acc509ef301607932cc914950d7bc
Richard Chapman <rchapman@hpccsystems.com>
2017-01-13 16:04:41 +0000

    More cleanup
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- restartJirabot.sh
+++ restartJirabot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/jira
-/bin/kill -9 `/bin/ps auxf | /bin/grep python | /bin/grep hello | /usr/bin/awk '{ print $2}'`
+cd ~/jirabot
+/bin/kill -9 `/bin/ps auxf | /bin/grep python | /bin/grep jirabot | /usr/bin/awk '{ print $2}'`
 /usr/bin/python2.7 jirabot.py 2>&1 > jirabot.out & 
